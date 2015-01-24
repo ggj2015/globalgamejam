e36ba800654967532c21995f6bc033d610dfedf0
imtiana <imjihyung@gmail.com>
2015-01-24 10:39:05 -0800

    score ui overlay

---
+++ ggj_simulator/Assets/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+struct TeamScore {
+	int totalScore;
+	int developer;
+	int audio;
+	int graphics;
+}
+
+public class ScoreManager : MonoBehaviour {
+
+	TeamScore player;
+	TeamScore enemyA;
+	TeamScore enemyB;
+	TeamScore enemyC;
+	TeamScore enemyD;
+
+	void Awake ()
+    {
+        // Set up the reference.
+        ScoreText = GetComponent <Text> ();
+
+        // Reset the score.
+        score = 0;
+    }
+
+    void Update ()
+    {
+        // Set the displayed text to be the word "Score" followed by the score value.
+        Text ScoreP.ScoreText.text = Score + player.totalScore;
+		//Text ScoreA.ScoreText.text = ;
+    
+    }
+}
