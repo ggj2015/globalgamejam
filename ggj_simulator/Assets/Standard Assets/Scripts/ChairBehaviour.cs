54f682ccf628ee9fe4661b9d066be2f351a9a002
whiteSkar <sypower07@hotmail.com>
2015-01-24 18:04:30 -0800

    Add chair mesh to chair

---
+++ ggj_simulator/Assets/Standard Assets/Scripts/ChairBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChairBehaviour : MonoBehaviour
+{
+
+	void Start ()
+    {
+        gameObject.renderer.transform.Rotate(new Vector3(270, 0, 0));
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
