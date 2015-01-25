3d6e6c5a731a6f4d9e769ff74e8d2dd6744d36fa
Sean Lee <sj.lee36bc@gmail.com>
2015-01-24 23:13:25 -0800

    SKIN

---
+++ ggj_simulator/Assets/Standard Assets/Scripts/GgjCoder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GgjCoder : GgjObject {
+
+    //void Awake() {
+        //(gameObject.GetComponent("Halo") as Behaviour).enabled = false;
+        //codeValue = 0.2f;
+    //}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
