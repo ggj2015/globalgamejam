fa1d7f955d0e0e667ad591501598e453166c4e14
Sean Lee <sj.lee36bc@gmail.com>
2015-01-24 17:48:16 -0800

    only highlkight items that are by the player's table

--- ggj_simulator/Assets/Standard Assets/Scripts/GgjObject.cs
+++ ggj_simulator/Assets/Standard Assets/Scripts/GgjObject.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class GgjObject : MonoBehaviour {
-    bool activated;
     float graphicsValue;
     float audioValue;
     float codeValue;
 
+    void Awake () {
+        (gameObject.GetComponent("Halo") as Behaviour).enabled = false;
+    }
+
 	// Use this for initialization
 	void Start () {
-
 	}
 	
 	// Update is called once per frame
@@ -18,11 +20,16 @@ public class GgjObject : MonoBehaviour {
 	}
 
     public void OnTriggerEnter(Collider other) {
-        (gameObject.GetComponent("Halo") as Behaviour).enabled = true;
+        // Only highlight if in the player's table area
+        string tableName = other.transform.parent.gameObject.ToString();
+        Debug.Log(tableName);
+
+        if (tableName == "Table (UnityEngine.GameObject)") { // HACK needs to change if table name changes
+            (gameObject.GetComponent("Halo") as Behaviour).enabled = true;
+        }
     }
 
     public void OnTriggerExit(Collider other) {
         (gameObject.GetComponent("Halo") as Behaviour).enabled = false;
-        Debug.Log(other.ToString());
     }
 }
